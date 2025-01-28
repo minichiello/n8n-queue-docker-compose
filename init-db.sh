3f0b93fa336cd55843207e4399a7aa28bbb19a13
Gerson Minichiello <gerson.minichiello@gmail.com>
2025-01-28 09:07:23 -0300

    Update init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Criar o usuário não-root se ele não existir
+# Create the non-root user if it does not exist
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   DO
   \$do\$
@@ -12,24 +12,24 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   END
   \$do\$;
 
-  -- Garantir que o usuário não-root tenha os privilégios necessários
+  -- Ensure the non-root user has the necessary privilegesecessários
   GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_NON_ROOT_USER;
   
-  -- Conectar ao banco n8n e conceder privilégios em todos os schemas futuros
+  -- Connect to the n8n database and grant privileges on all future schemas
   \c $POSTGRES_DB;
 
-  -- Garantir acesso ao schema public
+  -- Ensure access to the public schema
   GRANT ALL ON SCHEMA public TO $POSTGRES_NON_ROOT_USER;
   GRANT ALL ON ALL TABLES IN SCHEMA public TO $POSTGRES_NON_ROOT_USER;
   GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO $POSTGRES_NON_ROOT_USER;
   GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO $POSTGRES_NON_ROOT_USER;
   
-  -- Configurar privilégios padrão para objetos futuros
+  -- Set default privileges for future objects
   ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO $POSTGRES_NON_ROOT_USER;
   ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO $POSTGRES_NON_ROOT_USER;
   ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO $POSTGRES_NON_ROOT_USER;
   ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TYPES TO $POSTGRES_NON_ROOT_USER;
 
-  -- Garantir que o usuário possa criar no schema public
+  -- Ensure the user can create in the public schema
   ALTER SCHEMA public OWNER TO $POSTGRES_NON_ROOT_USER;
 EOSQL
